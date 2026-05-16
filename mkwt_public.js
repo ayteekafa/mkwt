a4fc3069600949f258d47447e0778ea707ea388d
ayteekafa <ayteekafa@gmail.com>
2026-05-16 23:10:24 +0200

    Bypass cached login scripts

--- mkwt_public.js
+++ mkwt_public.js
@@ -36,7 +36,7 @@
         }
         return;
       }
-      navigator.serviceWorker.register('/sw.js', { updateViaCache: 'none' }).catch(() => {
+      navigator.serviceWorker.register('/sw.js?v=577', { updateViaCache: 'none' }).catch(() => {
         navigator.serviceWorker.register('/sw.js').catch(() => {});
       });
     });
